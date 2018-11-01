6a0ee69109b807a467db860f7b377b9e471b88b3
Kostas Malliaridis <malliaridis@mentor.com.gr>
2018-11-01 14:12:58 +0200

    Update flights/index.php

--- flights/index.php
+++ flights/index.php
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php
 	include("./config.php");
 	include("./functions.php");
@@ -250,6 +251,8 @@
 >>>>>>> parent of 3bd17f4... Add files via upload
 =======
 >>>>>>> parent of 3bd17f4... Add files via upload
+=======
+>>>>>>> parent of 3bd17f4... Add files via upload
 <?php
 	include("./config.php");
 	include("./functions.php");
@@ -489,6 +492,9 @@
 </body>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 3bd17f4... Add files via upload
+=======
 >>>>>>> parent of 3bd17f4... Add files via upload
 =======
 >>>>>>> parent of 3bd17f4... Add files via upload
